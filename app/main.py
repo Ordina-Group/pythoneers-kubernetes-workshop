c76bb81928cfb0c792cf7763e4927136a14710c4
nahoo408 <nathan.hoogendorp@ordina.nl>
2024-10-23 13:08:29 +0200

    - Updated styling

--- app/main.py
+++ app/main.py
@@ -32,40 +32,47 @@ class Item(BaseModel):
     price: float
     quantity: int
 
+
 # In-memory "database"
 items = []
+
 
 # Root endpoint
 @app.get("/",  include_in_schema=False, response_class=HTMLResponse)
 async def read_root(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
 
+
 # Serve the favicon
 @app.get("/favicon.ico", include_in_schema=False)
 async def favicon():
     return FileResponse("../ico/python.ico")
 
-# Create (POST)
-@app.post("/items/", response_model=Item,  description="Add a new item to the list.",)
-async def create_item(item: Item):
-    items.append(item)
-    return item
 
 # Read (GET all)
-@app.get("/items", response_model=List[Item], description="Overview of all items.")
+@app.get("/items", response_model=List[Item], description="Overview of all items.",  tags=["Supermarket"])
 async def read_items():
     return items
 
+
 # Read (GET single item)
-@app.get("/items/{item_id}", response_model=Item, description="Get a specific item based on ID.")
+@app.get("/items/{item_id}", response_model=Item, description="Get a specific item based on ID.", tags=["Supermarket"])
 async def read_item(item_id: int):
     for item in items:
         if item.id == item_id:
             return item
     raise HTTPException(status_code=404, detail="Item not found")
 
+
+# Create (POST)
+@app.post("/items/", response_model=Item,  description="Add a new item to the list.", tags=["Supermarket"])
+async def create_item(item: Item):
+    items.append(item)
+    return item
+
+
 # Update (PUT)
-@app.put("/items/{item_id}", response_model=Item, description="Update an existing item based on ID.")
+@app.put("/items/{item_id}", response_model=Item, description="Update an existing item based on ID.", tags=["Supermarket"])
 async def update_item(item_id: int, updated_item: Item):
     for index, item in enumerate(items):
         if item.id == item_id:
@@ -73,8 +80,9 @@ async def update_item(item_id: int, updated_item: Item):
             return updated_item
     raise HTTPException(status_code=404, detail="Item not found")
 
+
 # Delete (DELETE)
-@app.delete("/items/{item_id}", description="Delete an existing item based on ID.")
+@app.delete("/items/{item_id}", description="Delete an existing item based on ID.", tags=["Supermarket"])
 async def delete_item(item_id: int):
     for index, item in enumerate(items):
         if item.id == item_id:
@@ -82,8 +90,9 @@ async def delete_item(item_id: int):
             return {"message": "Item deleted successfully"}
     raise HTTPException(status_code=404, detail="Item not found")
 
+
 # Read (GET all)
-@app.get("/crash", description="Crash the entire service in 5 seconds.", tags=["test"] )
+@app.get("/crash", description="Crash the entire service in 5 seconds.", tags=["crash"] )
 async def crash_app():
     # Countdown from 5 seconds before crashing
     for i in range(5, 0, -1):
@@ -94,8 +103,6 @@ async def crash_app():
     print("The application is about to crash!")
 
     os.kill(os.getpid(), signal.SIGTERM)
-    return Response(status_code=200, content='Server shutting down...')
-
 
 if __name__ == '__main__':
     import uvicorn
